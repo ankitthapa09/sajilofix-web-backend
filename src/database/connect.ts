54ddd0506d45e8be4fc5b4ea337880afd4f61f15
suyalthapa09@gmail.com <suyalthapa09@gmail.com>
2026-01-05 00:38:19 +0545

    backend connected with mongodb successfully

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -2,5 +2,10 @@ import mongoose from "mongoose";
 import { env } from "../config/env";
 
 export async function connectDatabase() {
+  if (mongoose.connection.readyState === 1) return;
+
   await mongoose.connect(env.MONGO_URI);
+
+  // eslint-disable-next-line no-console
+  console.log("MongoDB connected");
 }
